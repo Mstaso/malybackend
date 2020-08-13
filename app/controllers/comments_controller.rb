e371d3c69d5fe8c30a372a1cef6476643d4fd167
mstaso <mstaso1@gmail.com>
2020-08-13 17:08:10 -0400

    added create stuff for comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,5 +4,16 @@ class CommentsController < ApplicationController
         render json: @comments
     end 
 
+    def create
+        comment = Comment.create(comment_params)
+        render json: comment
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :post_id, :user_id)
+    end
+
 
 end
