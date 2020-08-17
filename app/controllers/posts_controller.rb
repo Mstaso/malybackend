034f05ee718ad7fb71ab8bc35f3a78e925c9428f
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-08-17 12:05:59 -0400

    user authentication done

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,4 +7,13 @@ class PostsController < ApplicationController
     def show 
         @post = Post.find_by[:id]
     end 
+
+    def create
+        post = Post.create(post_params)
+        render json: post
+    end
+
+    def post_params
+        params.require(:post).permit!
+    end
 end
