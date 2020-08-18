2c8997055c66ac652f30e961c1e4beaa984dbee0
MaleehaBhuiyan <maleehabhuiyan@baruchmail.cuny.edu>
2020-08-18 10:47:50 -0400

    got comments working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_12_200058) do
+ActiveRecord::Schema.define(version: 2020_08_17_201059) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 2020_08_12_200058) do
     t.string "content"
     t.integer "post_id"
     t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 ###USERS 
 
-u1 = User.create(username:"Maleehahaha", password_digest: "1234" )
+# u1 = User.create(username:"maleeha", password_digest: "1234" )
 
 ###POSTS 
 p1 = Post.create(name:"LINNMON / FINNVARD", 
@@ -116,9 +116,9 @@ p1 = Post.create(name:"LINNMON / FINNVARD",
     
 
  ###COMMENTS
-c1 = Comment.create(content:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit", 
-post_id:1, 
-user_id:1 ) 
+# c1 = Comment.create(content:"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit", 
+# post_id:1, 
+# user_id:1 ) 
 
-c2 = Comment.create(content:"nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur", post_id:2, 
-user_id:1 )
+# c2 = Comment.create(content:"nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur", post_id:2, 
+# user_id:1 )
